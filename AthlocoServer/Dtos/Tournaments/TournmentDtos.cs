13bd16b4f271d3c8caf3c72019cf185ed25d1dc7
Chris Rook <101030498+crookplusplus@users.noreply.github.com>
2024-04-02 12:24:07 -0700

    Build file commit

---
+++ AthlocoServer/Dtos/Tournaments/TournmentDtos.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AthlocoServer.Dtos.Tournaments
+{
+    public class TournmentDtos
+    {
+        
+    }
+}
--- AthlocoServer/obj/Debug/net8.0/AthlocoServer.AssemblyInfo.cs
+++ AthlocoServer/obj/Debug/net8.0/AthlocoServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AthlocoServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502dcf4a26d4554c1762f4900b696d2e3df36096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e79bf335c64098a83e376ef5b57bf69368c427")]
 [assembly: System.Reflection.AssemblyProductAttribute("AthlocoServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AthlocoServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

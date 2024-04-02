97c8907c454f044070623381d315d0de11b45918
Chris Rook <101030498+crookplusplus@users.noreply.github.com>
2024-04-02 14:21:37 -0700

    Added an update (PUT) endpoint to the API.

--- AthlocoServer/Controllers/TournamentController.cs
+++ AthlocoServer/Controllers/TournamentController.cs
@@ -50,6 +50,31 @@ namespace AthlocoServer.Controllers
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = tournamentModel.Id }, tournamentModel.toTournamentDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateTournamentRequestDto updateDto){
+            var tournamentModel = _context.Tournaments.FirstOrDefault(t => t.Id == id);
+
+            if (tournamentModel == null)
+            {
+                return NotFound();
+            }
+
+            tournamentModel.Title = updateDto.Title;
+            tournamentModel.Location = updateDto.Location;  
+            tournamentModel.Date = updateDto.Date;
+            tournamentModel.Type = updateDto.Type;
+            tournamentModel.TeamSize = updateDto.TeamSize;
+            tournamentModel.Cost = updateDto.Cost;
+            tournamentModel.CostPer = updateDto.CostPer;
+            tournamentModel.Style = updateDto.Style;
+            tournamentModel.Prize = updateDto.Prize;
+            tournamentModel.Host = updateDto.Host;
+
+            _context.SaveChanges();
+            return Ok(tournamentModel.toTournamentDto());
+        }
     
     }
 }
--- AthlocoServer/obj/Debug/net8.0/AthlocoServer.AssemblyInfo.cs
+++ AthlocoServer/obj/Debug/net8.0/AthlocoServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AthlocoServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0f967bbf1c930393efbcc4fd50c4c7c58471c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6992fd1c941d8a2f31491e60175b4ad96bd74917")]
 [assembly: System.Reflection.AssemblyProductAttribute("AthlocoServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AthlocoServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
